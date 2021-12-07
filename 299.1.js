ec611f603dc5b3cde79b8e30349a047b6417e71f
brurya-e <brurya.zak@gmail.com>
2021-12-07 20:21:29 +0200

    29.1- console log twice

--- 299.1.js
+++ 299.1.js
@@ -1,30 +1,26 @@
-// async function getIDs() {
-//         setTimeout(() => {
-//             return await([532, 543, 753, 1, 5]);
-//         }, 1500);
-//     }
-
 const getIDs = () =>
     new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve([532, 543, 753, 1, 5]);
         }, 1500);
     });
-    const getRecipe = (recipeID) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(
-                (ID) => {
-                    const recipe = {
-                        title: "Fresh tomato pasta",
-                        publisher: "Pinchas Hodadad",
-                    };
-                    resolve(`${ID}: ${recipe.title}`);
-                },
-                1500,
-                recipeID
-            );
-        });
-    };
+
+
+const getRecipe = (recipeID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            (ID) => {
+                const recipe = {
+                    title: "Fresh tomato pasta",
+                    publisher: "Pinchas Hodadad",
+                };
+                resolve(`${ID}: ${recipe.title}`);
+            },
+            1500,
+            recipeID
+        );
+    });
+};
 
 async function f() {
     const IDs = await getIDs()
@@ -38,7 +34,6 @@ f().catch((error) => {
 });
 
 f();
-
 
 // getIDs()
 //     .then((IDs) => {
