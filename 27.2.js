7a844e20b21d2435946acd1ca8b2c5217c792726
brurya-e <brurya.zak@gmail.com>
2021-12-07 16:03:27 +0200

    27.2 work

--- 27.2.js
+++ 27.2.js
@@ -8,34 +8,30 @@
 // in a .then, .catch.
 
 const makeAllCaps = (words) => {
-    for (let i = 0; i < words.length; i++) {
-        words[index] = words[index].toUpperCase();
-    }
-    console.log("caps");
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i].toUpperCase();
+        }
+        resolve(words)
+    })
 }
 
 const sortWords = (words) => {
     return new Promise((resolve, reject) => {
-        let temp = words.join('');
-        console.log(temp);
-        console.log(!/[^a-zA-Z]/.test(temp))
-        if ((/[^a-zA-Z]/.test(temp))){
-            console.log('no')
+        let temp = words.join('');  
+        if ((/[^a-zA-Z]/.test(temp))) {
             reject();
         }
         else {
-            console.log('yes')
             resolve(words.sort());
         }
     });
-    
+
 }
 
-arr = ['aa','bb', 'Asd'];
-arr2 = ['a23','bb', 'Asd']
+arr = ['aa', 'bb', 'Asd'];
+arr2 = ['a23', 'bb', 'Asd']
 sortWords(arr2)
-    .then((res) =>{ 
-        console.log(res)
-        makeAllCaps(res)})
-    // .then()
+    .then((res) => (makeAllCaps(res)))
+    .then((res) => console.log(res))
     .catch(() => { console.log("array not contains only strings") })
